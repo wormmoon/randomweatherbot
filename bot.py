5fb35128fbb3ba8cea6cc457d162edd1d2d423c4
Emily Oliver <mleeoliver@gmail.com>
2017-04-30 21:54:06 +0100

    Add import JSON

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 from secrets import *
 
@@ -7,7 +8,7 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth) 
 
 import forecastio
-api_key = "73670634fd8f73e49b11ba3497c4f12d"
+api_key = "73670634fd8f73e49b11ba3497c4f12d" #put this in secrets
 # Forecastio documentation says it requires long and lat info as well as the API key
 # But I want the long and lat to be different every time (different cities)
 # Plus I want the city name BUT I don't really want to enter this info manually
@@ -18,3 +19,5 @@ api_key = "73670634fd8f73e49b11ba3497c4f12d"
 # Tweet that out
 
 # wordcitiespop.txt has all the info for cities around the world and their long and lat
+
+# start by getting forcastio to take one long & lat and tweet out one weather? I guess? To see how that works
